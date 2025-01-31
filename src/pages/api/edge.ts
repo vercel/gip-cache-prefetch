870cdefb3d3131d962f171864c8c8ef73492638a
Luis Alvarez D <luis@vercel.com>
2025-01-31 14:25:08 -0500

    Removed some comments

--- src/pages/api/edge.ts
+++ src/pages/api/edge.ts
@@ -2,17 +2,11 @@ import type { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-// export const config = {
-// 	runtime: "edge",
-// };
-
 export default async function handler(req: NextRequest) {
 	const { searchParams } = new URL(req.url);
 	const path = searchParams.get("path") || "";
 
 	console.log("REQ", req.url, req.nextUrl);
-	// const redirect = Response.redirect(new URL(`/${path}`, req.url), 307);
-	// redirect.headers.set("x-redirect-custom", "1");
 
 	// This are all the headers you want to have be sent down to the browser.
 	const responseHeaders = new Headers({
